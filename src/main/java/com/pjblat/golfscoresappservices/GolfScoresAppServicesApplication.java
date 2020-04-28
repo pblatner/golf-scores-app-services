f10094b2cb175a73e4e504d5eb00bdcb86f31a70
Pete Blatner <pjblat@us.ibm.com>
2020-04-28 14:43:09 -0500

    core project files

---
+++ src/main/java/com/pjblat/golfscoresappservices/GolfScoresAppServicesApplication.java
@@ -0,0 +1,13 @@
+package com.pjblat.golfscoresappservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfScoresAppServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfScoresAppServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjblat/golfscoresappservices/GolfScoresAppServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjblat.golfscoresappservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfScoresAppServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
